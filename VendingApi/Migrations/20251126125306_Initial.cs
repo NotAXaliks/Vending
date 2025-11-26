14f82cc83e5f917ebdeee6b7ae48239c1c3235c9
NotAXaliks <vejmat276@gmail.com>
2025-11-26 16:48:39 +0300

    хз

---
+++ VendingApi/Migrations/20251126125306_Initial.cs
@@ -0,0 +1,322 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace VendingApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MachineModels",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Manufacturer = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Country = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MachineModels", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(70)", maxLength: 70, nullable: false),
+                    Description = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    Price = table.Column<decimal>(type: "numeric", nullable: false),
+                    InStock = table.Column<int>(type: "integer", nullable: false),
+                    MinStock = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    LastName = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    FirstName = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    MiddleName = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true),
+                    Email = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Phone = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: true),
+                    Role = table.Column<int>(type: "integer", nullable: false, defaultValue: 2),
+                    Password = table.Column<string>(type: "character varying(60)", maxLength: 60, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sessions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    RefreshToken = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false, defaultValueSql: "now() at time zone 'utc'"),
+                    ExpiresAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    RevokedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    IP = table.Column<string>(type: "text", nullable: true),
+                    DeviceName = table.Column<string>(type: "text", nullable: true),
+                    UserAgent = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sessions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Sessions_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Machines",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ModelId = table.Column<int>(type: "integer", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Location = table.Column<string>(type: "text", nullable: false),
+                    Address = table.Column<string>(type: "text", nullable: false),
+                    Coordinates = table.Column<string>(type: "text", nullable: true),
+                    PaymentType = table.Column<int>(type: "integer", nullable: false),
+                    TotalEarn = table.Column<decimal>(type: "numeric", nullable: false, defaultValue: 0m),
+                    SerialNumber = table.Column<string>(type: "character varying(40)", maxLength: 40, nullable: false),
+                    InventoryNumber = table.Column<string>(type: "character varying(40)", maxLength: 40, nullable: false),
+                    Modem = table.Column<string>(type: "text", nullable: true),
+                    WorkTime = table.Column<string>(type: "character varying(11)", maxLength: 11, nullable: true),
+                    Timezone = table.Column<int>(type: "integer", nullable: false, defaultValue: 15),
+                    Priority = table.Column<int>(type: "integer", nullable: false, defaultValue: 1),
+                    WorkMode = table.Column<int>(type: "integer", nullable: false, defaultValue: 0),
+                    Notes = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: true),
+                    EntryDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false, defaultValueSql: "now() at time zone 'utc'"),
+                    ManufactureDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    StartDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    LastMaintenanceDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    NextMaintenanceDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    InspectionIntervalMonths = table.Column<int>(type: "integer", nullable: true),
+                    ResourceHours = table.Column<int>(type: "integer", nullable: true),
+                    Status = table.Column<int>(type: "integer", nullable: false, defaultValue: 0),
+                    LastMaintenanceId = table.Column<int>(type: "integer", nullable: true),
+                    UsersId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Machines", x => x.Id);
+                    table.CheckConstraint("Machines_LastMaintenanceDate_check", "\"LastMaintenanceDate\" >= \"EntryDate\" and \"LastMaintenanceDate\" <= now()");
+                    table.CheckConstraint("Machines_NextMaintenanceDate_check", "\"NextMaintenanceDate\" >= \"EntryDate\" and \"NextMaintenanceDate\" <= now()");
+                    table.CheckConstraint("Machines_StartDate_check", "\"StartDate\" >= \"EntryDate\" and \"StartDate\" > \"ManufactureDate\"");
+                    table.ForeignKey(
+                        name: "FK_Machines_MachineModels_ModelId",
+                        column: x => x.ModelId,
+                        principalTable: "MachineModels",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Machines_Users_UsersId",
+                        column: x => x.UsersId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Maintenances",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    MachineId = table.Column<int>(type: "integer", nullable: false),
+                    Date = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    DurationHours = table.Column<int>(type: "integer", nullable: false),
+                    ActualDurationSeconds = table.Column<int>(type: "integer", nullable: false),
+                    Description = table.Column<string>(type: "character varying(1500)", maxLength: 1500, nullable: false),
+                    Issues = table.Column<string>(type: "character varying(700)", maxLength: 700, nullable: true),
+                    PerformedById = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Maintenances", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Maintenances_Machines_MachineId",
+                        column: x => x.MachineId,
+                        principalTable: "Machines",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Maintenances_Users_PerformedById",
+                        column: x => x.PerformedById,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sales",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    MachineId = table.Column<int>(type: "integer", nullable: false),
+                    ProductId = table.Column<int>(type: "integer", nullable: false),
+                    Quantity = table.Column<int>(type: "integer", nullable: false),
+                    Amount = table.Column<decimal>(type: "numeric", nullable: false),
+                    Date = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false, defaultValueSql: "now() at time zone 'utc'"),
+                    PaymentMethod = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sales", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Sales_Machines_MachineId",
+                        column: x => x.MachineId,
+                        principalTable: "Machines",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Sales_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Machines_InventoryNumber",
+                table: "Machines",
+                column: "InventoryNumber",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Machines_LastMaintenanceId",
+                table: "Machines",
+                column: "LastMaintenanceId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Machines_ModelId",
+                table: "Machines",
+                column: "ModelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Machines_Name",
+                table: "Machines",
+                column: "Name");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Machines_SerialNumber",
+                table: "Machines",
+                column: "SerialNumber",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Machines_UsersId",
+                table: "Machines",
+                column: "UsersId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Maintenances_Date",
+                table: "Maintenances",
+                column: "Date",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Maintenances_MachineId",
+                table: "Maintenances",
+                column: "MachineId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Maintenances_PerformedById",
+                table: "Maintenances",
+                column: "PerformedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sales_MachineId",
+                table: "Sales",
+                column: "MachineId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sales_ProductId",
+                table: "Sales",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sessions_UserId",
+                table: "Sessions",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Phone",
+                table: "Users",
+                column: "Phone",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Machines_Maintenances_LastMaintenanceId",
+                table: "Machines",
+                column: "LastMaintenanceId",
+                principalTable: "Maintenances",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Machines_MachineModels_ModelId",
+                table: "Machines");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Machines_Maintenances_LastMaintenanceId",
+                table: "Machines");
+
+            migrationBuilder.DropTable(
+                name: "Sales");
+
+            migrationBuilder.DropTable(
+                name: "Sessions");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "MachineModels");
+
+            migrationBuilder.DropTable(
+                name: "Maintenances");
+
+            migrationBuilder.DropTable(
+                name: "Machines");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
